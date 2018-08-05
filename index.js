764165afdf2151e0abb0a3a8174a061c373a8094
Patrick Valer <hello@patrickvaler.ch>
2018-08-05 04:39:50 +0200

    feat(all): make driver addable

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const dysonTp04Driver = require('./lib');
-
-module.exports = {
-  devices: [
-    // Exports listed here will be detected,
-    // these are the objects returned by neeo-sdk.buildDevice()
-    dysonTp04Driver
-  ]
-};
+module.exports = require('./lib');
